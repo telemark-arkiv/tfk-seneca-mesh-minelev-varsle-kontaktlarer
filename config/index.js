956d92a944888d01ff0b8b9a88d489eb345f40f1
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-08-10 13:42:20 +0200

    Adds docker

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@
 const envs = process.env
 
 module.exports = {
-  JWT_KEY: envs.TFK_SENECA_MESH_MINELEV_VARSLE_KONTAKTLARER_JWT_KEY || 'Louie Louie, oh no, I got to go. Louie Louie, oh no, I got to go',
+  JWT_KEY: envs.TFK_SENECA_MINELEV_VARSLE_KONTAKTLARER_JWT_KEY || 'Louie Louie, oh no, I got to go. Louie Louie, oh no, I got to go',
   BUDDY_URL: envs.BUDDY_URL || 'https://api.buddy.com'
 }
